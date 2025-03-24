c8cd0b79940a469967a7f9e7c26a0b0d94c4a715
root <root@ip-172-31-39-238.eu-central-1.compute.internal>
2025-03-24 10:08:52 +0000

    agreements sayfası div düzenlemesi

--- resources/views/site/agreements.blade.php
+++ resources/views/site/agreements.blade.php
@@ -3,7 +3,7 @@
 <div class="container mx-auto mt-30">
 <div class="p-5">
     <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center">{{ $agreements['title'] }}</h1>
-    <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">{!! nl2br($agreements['description']) !!}</p>
+    <div class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">{!! nl2br($agreements['description']) !!}</div>
 </div>
 </div>
 
